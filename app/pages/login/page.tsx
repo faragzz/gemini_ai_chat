cb9ebf89abe2ecbac3673abaaf1c2c9b2162ef8d
faragzz <a.khaled46462@gmail.com>
2024-02-02 01:39:34 +0200

    first commit

---
+++ app/pages/login/page.tsx
@@ -0,0 +1,13 @@
+// pages/about.js
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>
+            <h1>Login Page</h1>
+            {/* Add content for the About page */}
+        </div>
+    );
+};
+
+export default Page;
